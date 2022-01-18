697ecdfbf1af6d229a5f8486df5f89d6bce9a970
chandlerwillis <58713815+chandlerwillis@users.noreply.github.com>
2022-01-18 11:45:50 -0500

    Add files via upload

---
+++ pythonworkshop_stack.py
@@ -0,0 +1,13 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+)
+
+
+class CdkWorkshopStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Nothing here!
+
